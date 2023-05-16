417870595e013995440c0b674f7fba854c03c642
nguyenduckhanh2003 <133756583+nguyenduckhanh2003@users.noreply.github.com>
2023-05-16 16:03:22 +0700

    Update bai3.java

--- bai3.java
+++ bai3.java
@@ -3,14 +3,38 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package slot3;
+package slot.pkg3;
 
 /**
  *
- * @author Dell                 
+ * @author PC
  */
-public class bai3 {
-       public static void main(String[] args) {
-           
-       }
+public class Peterson1to1000 {
+     public static void main(String[] args) {
+        System.out.println("Các số Peterson từ 1 đến 1000:");
+        for (int i = 1; i <= 1000; i++) {
+            if (isPetersonNumber(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPetersonNumber(int number) {
+        int sum = 0;
+        int temp = number;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+        return sum == number;
+    }
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
 }
