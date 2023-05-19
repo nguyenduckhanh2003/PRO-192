8fd3c9a1a0b71d4c1cd765a3a8709ed24b9e3747
nguyenduckhanh2003 <133756583+nguyenduckhanh2003@users.noreply.github.com>
2023-05-19 17:09:27 +0700

    Update bai3.java

--- slot3/src/slot3/bai3.java
+++ slot3/src/slot3/bai3.java
@@ -7,10 +7,34 @@ package slot3;
 
 /**
  *
- * @author Dell                 
+ * @author PC
  */
 public class bai3 {
-       public static void main(String[] args) {
-           
-       }
+     public static void main(String[] args) {
+        System.out.println("Các số Peterson từ 1 đến 1000:");
+        for (int i = 1; i <= 1000; i++) {
+            if (isPetersonNumber(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPetersonNumber(int number) {
+        int sum = 0;
+        int temp = number;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+        return sum == number;
+    }
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
 }
